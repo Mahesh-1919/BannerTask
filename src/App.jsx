f5d97052b07d23a7d49902dd38d302e4ca4fb4a9
Mahesh-1919 <maheshjyothula23@gmail.com>
2024-08-12 12:33:57 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/navBar";
 import Banner from "./components/banner";
 import Products from "./components/products";
 import { useRecoilValue } from "recoil";
